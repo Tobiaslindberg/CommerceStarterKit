8092ae7f875d557c801b3dd936fcfbc5ab02114c
evest <steve@celius.cc>
2014-12-01 14:35:51 +0100

    Fixed links to products and logo
    Product image thumbnail in detail view is not shown as an actual
    thumbnail. Clicking logo in top preserves the current language
    (previously always reverted back to English.)

---
+++ src/web/Extensions/UrlExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
+
+namespace OxxCommerceStarterKit.Web.Extensions
+{
+    public static class UrlExtentions
+    {
+        public static string GetDefaultModeUrl(this UrlResolver urlResolver, ContentReference contentReference)
+        {
+            return urlResolver.GetUrl(contentReference, null, new VirtualPathArguments() { ContextMode = ContextMode.Default });
+        }
+    }
+}
