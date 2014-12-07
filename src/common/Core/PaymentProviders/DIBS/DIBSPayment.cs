3a4e7cce19640fc42803f8615f6e2e1b71ece2d3
evest <steve@celius.cc>
2014-12-07 20:17:08 +0100

    Removed entity framework packages

---
+++ src/common/Core/PaymentProviders/DIBS/DIBSPayment.cs
@@ -0,0 +1,10 @@
+namespace OxxCommerceStarterKit.Core.PaymentProviders.DIBS
+{
+    public static class DIBSPayment
+    {
+        public const string SessionLatestOrderIdKey = "LatestOrderId";
+        public const string SessionLatestOrderNumberKey = "LatestOrderNumber";
+        public const string SessionLatestOrderTotalKey = "LatestOrderTotal";
+        public const string DIBSSystemName = "DIBS";
+    }
+}
